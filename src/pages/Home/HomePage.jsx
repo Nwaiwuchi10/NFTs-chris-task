8b78519f4ab7ea6b3c7e843a3d6cc0e7e661bc29
Nwaiwuchi10 <djnchrys@gmail.com>
2023-03-18 17:52:00 -0700

    mdkkdfh

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -3,11 +3,13 @@ import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 
 import Card from "../../components/NTFsCards/Card";
+import Contact from "../../components/Contact/Contact";
 const HomePage = () => {
   return (
     <div>
       <Header />
       <Card />
+      <Contact />
       <Footer />
     </div>
   );
